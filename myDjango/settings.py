7bf24313506e0c21fba18d30f31e9f9ac6154e37
hari@alokin-lap <harikrishnanb@alokin.in>
2021-04-06 15:34:40 +0530

    App/Component products added

--- myDjango/settings.py
+++ myDjango/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Party
+    
+    # Own
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local import *
+except:
+    pass
